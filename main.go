7c3a8291576a43c5296c572e01bb5de26ba8ceaf
Hiroto Shioi <shioihigg@gmail.com>
2023-03-07 17:12:53 +0900

    Load config

--- main.go
+++ main.go
@@ -6,18 +6,17 @@ import (
 
 	"github.com/HirotoShioi/simplebank/api"
 	db "github.com/HirotoShioi/simplebank/db/sqlc"
+	"github.com/HirotoShioi/simplebank/util"
 	_ "github.com/lib/pq"
-)
-
-const (
-	dbDriver      = "postgres"
-	dbSource      = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
-	serverAddress = "0.0.0.0:8081"
 )
 
 func main() {
 
-	conn, err := sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("cannot read config file")
+	}
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
 	}
@@ -25,7 +24,7 @@ func main() {
 	store := db.NewStore(conn)
 	server := api.NewServer(store)
 
-	err = server.Start(serverAddress)
+	err = server.Start(config.ServerAddress)
 	if err != nil {
 		log.Fatal("cannot start server:", err)
 	}
